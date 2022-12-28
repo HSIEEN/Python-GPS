816fe11c40356731364334b8a4cbe9306936fb67
HSIEEN <87420780+HSIEEN@users.noreply.github.com>
2022-12-28 16:40:43 +0800

    Update HausdorffDist.py
    modified

--- HausdorffDist.py
+++ HausdorffDist.py
@@ -10,58 +10,58 @@ def HausdorffDist(P=None, Q=None, dv=None):
     if not (sP[1] == sQ[1]):
         raise Exception('Inputs P and Q must have the same number of columns')
     # interpolation for measured data
-    ssd = Q.shape
-    Q_inter = np.array([Q[0]])
-    # Q_inter = np.zeros((1, 2))
+    # ssd = Q.shape
+    # Q_inter = np.array([Q[0]])
+    # Q = np.zeros((1, 2))
     # interpolation among sparse points
-    if ssd[0] > 1 and ssd[1] == 2:
-        max_inter = 3
-        pad_idx = 0
-        for i in range(1, ssd[0] - 1):
-            pad_num = 1
-            pad_idx = pad_idx + 1
-            Q_inter = np.append(Q_inter, [Q[i]], axis=0)  # Q_inter[pad_idx, :] = Q[i, :]
-            inter = np.sqrt(sum((Q[i] - Q[i + 1]) ** 2))
-            while inter > pad_num * max_inter:
-                pad_num = pad_num + 1
-                pad_idx = pad_idx + 1
-                Q_inter = np.append(
-                    Q_inter, [Q_inter[pad_idx - 1] + (Q[i + 1] - Q[i]) * max_inter / inter],
-                    axis=0)
-                # Q_inter[pad_idx, :] = Q_inter[pad_idx - 1, :] + (Q[i + 1, :] - Q[i, :]) * max_inter /
-                # inter
-        Q_inter = np.append(Q_inter, [Q[-1]], axis=0)
-    sQ = Q_inter.shape
-    mp = np.zeros((sP[0], 1))
-    ixp = np.ones((sP[0], 1))
-    for i in range(0, sP[0]):
-        mp[i] = 10000000000.0
-        for j in range(0, sQ[0]):
-            dist = np.sqrt(sum((Q_inter[j, :] - P[i, :]) ** 2))
-            if mp[i] > dist:
-                mp[i] = dist
-                ixp[i] = j
+    # if ssd[0] > 1 and ssd[1] == 2:
+    #     max_inter = 3
+    #     pad_idx = 0
+    #     for i in range(1, ssd[0] - 1):
+    #         pad_num = 1
+    #         pad_idx = pad_idx + 1
+    #         Q_inter = np.append(Q_inter, [Q[i]], axis=0)  # Q_inter[pad_idx, :] = Q[i, :]
+    #         inter = np.sqrt(sum((Q[i] - Q[i + 1]) ** 2))
+    #         while inter > pad_num * max_inter:
+    #             pad_num = pad_num + 1
+    #             pad_idx = pad_idx + 1
+    #             Q_inter = np.append(
+    #                 Q_inter, [Q_inter[pad_idx - 1] + (Q[i + 1] - Q[i]) * max_inter / inter],
+    #                 axis=0)
+    #             # Q_inter[pad_idx, :] = Q_inter[pad_idx - 1, :] + (Q[i + 1, :] - Q[i, :]) * max_inter /
+    #             # inter
+    #     Q_inter = np.append(Q_inter, [Q[-1]], axis=0)
+    # sQ = Q.shape
+    # mp = np.zeros((sP[0], 1))
+    # ixp = np.ones((sP[0], 1))
+    # for i in range(0, sP[0]):
+    #     mp[i] = 10000000000.0
+    #     for j in range(0, sQ[0]):
+    #         dist = np.sqrt(sum((Q[j, :] - P[i, :]) ** 2))
+    #         if mp[i] > dist:
+    #             mp[i] = dist
+    #             ixp[i] = j
 
     mq = np.zeros((sQ[0], 1))
     ixq = np.ones((sQ[0], 1))
     for i in range(0, sQ[0]):
         mq[i] = 10000000000.0
         for j in range(0, sP[0]):
-            dist = np.sqrt(sum((Q_inter[i, :] - P[j, :]) ** 2))
+            dist = np.sqrt(sum((Q[i, :] - P[j, :]) ** 2))
             if mq[i] > dist:
                 mq[i] = dist
                 ixq[i] = j
 
-    vp = np.mean(mp)
+    # vp = np.mean(mp)
 
     vq = np.mean(mq)
 
-    mhd = max(vp, vq)
-    maxp = np.amax(mp)
+    # mhd = max(vp, vq)
+    # maxp = np.amax(mp)
     # maxp, ixpp = np.amax(mp)
     maxq = np.amax(mq)
     # maxq, ixqq = np.amax(mq)
-    hd = np.amax(np.array([maxq, maxp]))
+    # hd = np.amax(np.array([maxq, maxp]))
     ##
     # -- plot data --
     #
@@ -93,4 +93,4 @@ def HausdorffDist(P=None, Q=None, dv=None):
     # plt.title(np.array([[np.array(['Max Distance = ',num2str(hd)])],[np.array(['Mean Distance = ',num2str(mhd)])],[[]]]))
     # plt.legend(h,np.array(['P','Q','Maximum Dist']),'location','eastoutside')
 
-    return hd, mhd
+    return maxq, vq
